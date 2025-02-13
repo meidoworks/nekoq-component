2dda91d1b6c0d185ca8c5414aaf29246c760730a
goodplayer <meidomx@gmail.com>
2025-02-13 23:28:10 +0800

    Refactor ClientAdv api using generics

--- configure/configclient/client.adv.go
+++ configure/configclient/client.adv.go
@@ -11,29 +11,36 @@ import (
 
 type Unmarshaler func([]byte, interface{}) error
 
-type ClientAdv struct {
+type ClientAdv[T any] struct {
 	c *Client
+
+	// OnChange is called when any update on the configuration
+	OnChange func(cfg configapi.Configuration, container T)
 }
 
-func NewClientAdv(c *Client) *ClientAdv {
-	return &ClientAdv{c: c}
+func NewClientAdv[T any](c *Client) *ClientAdv[T] {
+	return &ClientAdv[T]{c: c}
 }
 
 // RegisterJsonContainer will register auto updated configure container with json configure support
 // Note: the behavior is the same as Register method
-func (c *ClientAdv) RegisterJsonContainer(group, key string, container any) (*atomic.Value, error) {
-	return c.Register(group, key, json.Unmarshal, container)
+func (c *ClientAdv[T]) RegisterJsonContainer(group, key string) (*ConfigContainer[T], error) {
+	return c.Register(group, key, json.Unmarshal)
 }
 
 // Register will register auto updated configure container with the same type as the container provided
 // Note that any further update has to be accessed via responded container rather than the container provided in the parameter
-func (c *ClientAdv) Register(group, key string, unmarshaler Unmarshaler, container any) (*atomic.Value, error) {
-	if !checkStructPtr(container) {
+func (c *ClientAdv[T]) Register(group, key string, unmarshaler Unmarshaler) (*ConfigContainer[T], error) {
+	var dummy T
+	if !checkStructPtr(dummy) {
 		return nil, errors.New("container parameter should be '*struct' type")
 	}
-	structType := getStructType(container)
-	result := new(atomic.Value)
-	result.Store(newStructPtr(structType))
+	structType := getStructType(dummy)
+	val := new(atomic.Value)
+	val.Store(newStructPtr(structType))
+	result := new(ConfigContainer[T])
+	result.val = val
+	result.OnChange = c.OnChange
 	c.c.suspend()
 	defer c.c.resume()
 
@@ -49,7 +56,11 @@ func (c *ClientAdv) Register(group, key string, unmarshaler Unmarshaler, contain
 				// guarantee changes are applied successfully before start using container as configure
 				c.c.logError("unmarshal ClientAdv change failed", err)
 			} else {
-				result.Store(newInst)
+				val.Store(newInst)
+			}
+			onchange := result.OnChange
+			if onchange != nil {
+				onchange(cfg, newInst.(T))
 			}
 		},
 	})
@@ -66,51 +77,6 @@ type ConfigContainer[T any] struct {
 
 func (cc *ConfigContainer[T]) Get() T {
 	return cc.val.Load().(T)
-}
-
-// Register will register auto updated configure container with the same type as the container provided
-// Note that any further update has to be accessed via responded container rather than the container provided in the parameter
-func (cc *ConfigContainer[T]) Register(c *ClientAdv, group, key string, unmarshaler Unmarshaler) error {
-	var structTypeSlice []T
-	t := getSliceItemType(structTypeSlice)
-	if !checkStructPtrOnType(t) {
-		return errors.New("container parameter should be '*struct' type")
-	}
-	structType := getStructTypeOnType(t)
-	result := new(atomic.Value)
-	result.Store(newStructPtr(structType))
-	cc.val = result
-	c.c.suspend()
-	defer c.c.resume()
-
-	c.c.AddConfigurationRequirement(RequiredConfig{
-		Required: configapi.RequestedConfigurationKey{
-			Group: group,
-			Key:   key,
-		},
-		Callback: func(cfg configapi.Configuration) {
-			newInst := newStructPtr(structType)
-			if err := unmarshaler(cfg.Value, newInst); err != nil {
-				//FIXME need better solution to alert callback error
-				// guarantee changes are applied successfully before start using container as configure
-				c.c.logError("unmarshal ClientAdv change failed", err)
-			} else {
-				result.Store(newInst)
-			}
-			onchange := cc.OnChange
-			if onchange != nil {
-				onchange(cfg, newInst.(T))
-			}
-		},
-	})
-
-	return nil
-}
-
-// RegisterJsonContainer will register auto updated configure container with json configure support
-// Note: the behavior is the same as Register method
-func (cc *ConfigContainer[T]) RegisterJsonContainer(c *ClientAdv, group, key string) error {
-	return cc.Register(c, group, key, json.Unmarshal)
 }
 
 func getSliceItemType(slice any) reflect.Type {
--- go.mod
+++ go.mod
@@ -1,17 +1,19 @@
 module github.com/meidoworks/nekoq-component
 
-go 1.23.0
+go 1.24.0
 
 require (
 	github.com/fxamacker/cbor/v2 v2.7.0
-	github.com/go-chi/chi/v5 v5.2.0
+	github.com/go-chi/chi/v5 v5.2.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/peterbourgon/diskv/v3 v3.0.1
 	github.com/spf13/afero v1.12.0
-	go.etcd.io/bbolt v1.3.11
-	go.etcd.io/etcd/client/v3 v3.5.17
-	golang.org/x/crypto v0.32.0
+	go.etcd.io/bbolt v1.4.0
+	go.etcd.io/etcd/client/v3 v3.5.18
+	golang.org/x/crypto v0.33.0
+	gorm.io/driver/postgres v1.5.11
+	gorm.io/gorm v1.25.12
 )
 
 require (
@@ -23,17 +25,19 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	go.etcd.io/etcd/api/v3 v3.5.17 // indirect
-	go.etcd.io/etcd/client/pkg/v3 v3.5.17 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.18 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.18 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20250106144421-5f5ef82da422 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250106144421-5f5ef82da422 // indirect
-	google.golang.org/grpc v1.69.2 // indirect
-	google.golang.org/protobuf v1.36.2 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250207221924-e9438ea467c6 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250207221924-e9438ea467c6 // indirect
+	google.golang.org/grpc v1.70.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
